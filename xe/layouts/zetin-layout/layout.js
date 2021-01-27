2cb7d902c3f64f7c95714dcb1565216a1f77739b
shythm <55665829+shythm@users.noreply.github.com>
2021-01-28 04:24:31 +0900

    Release 2021-01-28

--- xe/layouts/zetin-layout/layout.js
+++ xe/layouts/zetin-layout/layout.js
@@ -1,3 +1,40 @@
 jQuery(function ($) {
 
+    /*
+    // 반응형 js
+
+    function respHandler(mql) {
+        if (mql.matches) {
+            // mobile
+            $('.mobile-only').show();
+            $('.desktop-only').hide();
+        } else {
+            // desktop
+            $('.desktop-only').show();
+            $('.mobile-only').hide();
+        }
+    }
+    
+    var mql = window.matchMedia("screen and (max-width: 720px)");
+    mql.addEventListener("change", function (e) {
+        respHandler(e);
+    });
+    respHandler(mql);
+    */
+
+    function getUrlParams() {
+        var params = {};
+        window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+            function (str, key, value) {
+                params[key] = value;
+            });
+        return params;
+    }
+
+    var urlParams = getUrlParams();
+    if (urlParams.mid == 'home' && urlParams.act) {
+        // 홈페이지에서 부가 기능 실행 시 홈페이지 내용 숨기기
+        $('.zetin-home').hide();
+    }
+
 });
